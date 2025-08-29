7fb3e34de2d23704b1a8f318188f1e8e0d1522e1
Lina María <linaramirezm11@gmail.com>
2025-08-29 17:02:42 -0500

    [Add] Implement global error handling with custom error attributes and exception handler

--- infrastructure/entry-points/reactive-web/src/test/java/co/com/crediya/api/config/ConfigTest.java
+++ infrastructure/entry-points/reactive-web/src/test/java/co/com/crediya/api/config/ConfigTest.java
@@ -1,6 +1,6 @@
 package co.com.crediya.api.config;
 
-import co.com.crediya.api.Handler;
+import co.com.crediya.api.LoanApplicationHandler;
 import co.com.crediya.api.RouterRest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@ContextConfiguration(classes = {RouterRest.class, Handler.class})
+@ContextConfiguration(classes = {RouterRest.class, LoanApplicationHandler.class})
 @WebFluxTest
 @Import({CorsConfig.class, SecurityHeadersConfig.class})
 class ConfigTest {
