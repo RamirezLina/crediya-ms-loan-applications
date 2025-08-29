317386171c646bde0b0553c8727f096752e4e6a6
Lina María <linaramirezm11@gmail.com>
2025-08-29 17:02:00 -0500

    [Add] Implement loan application registration handler and routing

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/RouterRest.java
@@ -0,0 +1,24 @@
+package co.com.crediya.api;
+
+import co.com.crediya.api.config.LoanApplicationPath;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+@RequiredArgsConstructor
+public class RouterRest {
+    
+    private final LoanApplicationPath loanApplicationPath;
+    private final LoanApplicationHandler handler;
+    
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction(LoanApplicationHandler loanApplicationHandler) {
+        return route(POST( loanApplicationPath.getLoanApp()), loanApplicationHandler::listenRegisterLoanApplication);
+    }
+}
